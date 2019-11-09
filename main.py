f6253f4ea502bc419d8a752663850b7c2cc8251d
Andrew Riachi <brasswood@users.noreply.github.com>
2019-11-09 11:28:03 -0600

    yaml parser ready

--- main.py
+++ main.py
@@ -1,23 +1,8 @@
 import prompt
-PROMPTS=["Hello! Thank you for contacting the suicide hotline. First off, you've made the right decision.\nWhile waiting for a volunteer to become available, could you answer some important questions for us?", 
-        "Do you have a plan to attempt suicide?", 
-        "When were you planning to kill yourself?", 
-        "Where would you do it?", 
-        "What would you use?", 
-        "Where is the weapon? How many pills do you have? Gun loaded?", 
-        "Have you ever tried to take your life before? When? What happened?", 
-        "What's happened in your life that makes you want to end everything?", 
-        "Who have you talked with about wanting to end your life?", 
-        "On a scale of 1-10, (1 being normally upbeat, 10 being extremely depressed or hopeless) how would you say you feel?", 
-        "What might you feel your suicide would help you to do?", 
-        "Do you know someone who has died by suicide?", 
-        "Are you currently drinking alcohol or using drugs?", 
-        "Okay. Thank you for responses, they will allow us to give you more personalized care. We are about to connect you with a volunteer. It'll be about 3 more minutes."]
 
-responses = []
 # Initial value for our linked list
-p = prompt.LIST[0]
-while (p.next_prompt != -1):
+p = prompt.prompt_list('start')
+while (p.next_prompt != 0):
     user_in = input(p.message + '\n')
     while (user_in not in p.acceptable_responses):
         print("Invalid response")
@@ -26,4 +11,4 @@ while (p.next_prompt != -1):
             s += (r + '/') 
         s += ")\n"
         user_in = input(s)
-    p = prompt.LIST[p.next_prompt] # p = next value in our linked list
+    p = prompt.prompt_list(p.next_prompt) # p = next value in our linked list
--- prompt.py
+++ prompt.py
@@ -1,23 +1,22 @@
+import yaml
+
+objdict = {}
+
 class Prompt:
     """A class that contains a prompt message, acceptable responses,
     and a next_prompt"""
     def __init__(self, message, acceptable_responses, next_prompt):
         self.message = message
         self.acceptable_responses = acceptable_responses
-        self.next_prompt = next_prompt # a function of response
+        self.next_prompt = next_prompt # a string that matches a key in objdict 
 
-LIST = [
-        Prompt("Hello! Thank you for contacting the suicide hotline. First off, you've made the right decision.", [''], 1), # 0
-        Prompt("While waiting for a volunteer to become available, could you answer some important questions for us?", ['yes', 'no'], 2), # 1
-        Prompt("Do you have a plan to attempt suicide?", ['yes', 'no'], 3), # 2
-        Prompt("When were you planning to kill yourself?", ['today', 'tomorrow', "i don't know"], 4), # 3
-        Prompt("Where would you do it?", ['house', "i don't know"], -1), # 4
-        '''
-        Prompt("Where are the <pills, gun, knife>? How many pills do you have? Gun loaded?"), # 5
-        Prompt("Have you ever tried to take your life before? When? What happened?"), # 6
-        Prompt("What's happened in your life that mkaes you want to end everything?"), # 7
-        Prompt("Do you know someone who has died by suicide?"), # 8
-        Prompt("Are you currently drinking alcohol or using drugs?"), # 9
-        Prompt("Okay. Thank you for your responses, they will allow us to better serve your needs. We are about to connect you with a volunteer. It'll be about 3 more minutes.") # 10
-        '''
-        ]
+def load(filename):
+    with open(filename) as f:
+        d = yaml.load(f)
+    for key, value in d.items():
+        objdict[key] = Prompt(value['message'], value['acceptable_responses'], value['next_prompt'])
+
+def prompt_list(key):
+    if not objdict:
+        load('data.yml')
+    return (objdict[key])
